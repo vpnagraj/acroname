050c7586d98984d545155db294bf435428814c42
VP Nagraj <nagraj@nagraj.net>
2021-01-17 11:13:28 -0500

    adding tests for initialism()

---
+++ tests/testthat/test-engines.R
@@ -0,0 +1,12 @@
+test_that("inititalism captures first chars", {
+
+  tst <- initialism("the fox condemns the trap not himself")
+  expect_equal(tst, "FCTNH: Fox Condemns Trap Not Himself")
+
+  tst <- initialism("the fox condemns the trap not himself", ignore_articles = FALSE)
+  expect_equal(tst, "TFCTTNH: The Fox Condemns The Trap Not Himself")
+
+  tst <- initialism("Nothing compares 2 u")
+  expect_equal(tst, "NC2U: Nothing Compares 2 U")
+
+})
